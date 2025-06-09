938bef0dc26c9eac6ba97a60fed6f76dd064f33d
Maxence Rosseel <144241878+maxence-rosseel@users.noreply.github.com>
2025-06-09 13:50:26 +0200

    loading screen fonctionnel

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,33 @@
 import '../style/Home.css'
+import '../style/App.css'
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  return (
+const [connected, setConnected] = useState<boolean>(false);
+
+  useEffect(() => {
+    fetch('https://testcirmon.onrender.com/test') // remplace par ton URL backend
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) setConnected(true);
+      })
+      .catch(() => {
+        setConnected(false)
+        console.log("BDD Connectée");
+      });
+  }, []);
+
+  if (connected) {
+    return (
     <div id="page-container">Bienvenue sur la page d'accueil
       <div>blablabla</div>
-    </div>
-  );
+    </div>);
+  }
+  else {
+    return (
+      <div id="page-container-loading">
+        <img className="loadingImg"  src="img/illustrations/car.jpg" alt="car"/>
+      </div>
+    );
+  }
 }
