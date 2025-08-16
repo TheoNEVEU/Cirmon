e3f2ff4ade817e37370463d9b472699e694b46d7
TheoNEVEU <theo.neveu@student.junia.com>
2025-08-16 20:47:33 +0200

    ca me les brise

--- backend/index.js
+++ backend/index.js
@@ -105,11 +105,8 @@ app.post('/login', async (req, res) => {
 // Récupération de toutes les infos du compte
 app.get('/users', async (req, res) => {
   const authHeader = req.headers['authorization'];
-
   if (!authHeader) return res.status(401).json({ success: false, message: 'Token manquant' });
-
   const token = authHeader.split(' ')[1];
-
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
